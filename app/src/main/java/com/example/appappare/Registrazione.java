de6781d94b798c3916926165924a4b0ebf376c32
enrGar <garau95@gmail.com>
2019-05-11 18:54:32 +0200

    Elimiazione della schermata di scelta tra login e registrazione, aggiunta Intent e gestione login

--- app/src/main/java/com/example/appappare/Registrazione.java
+++ app/src/main/java/com/example/appappare/Registrazione.java
@@ -3,11 +3,11 @@ package com.example.appappare;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginPage extends AppCompatActivity {
+public class Registrazione extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_page);
+        setContentView(R.layout.activity_registrazione);
     }
 }
--- app/src/main/java/com/example/appappare/effettuaLogin.java
+++ app/src/main/java/com/example/appappare/effettuaLogin.java
@@ -1,13 +1,71 @@
 package com.example.appappare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 
 public class effettuaLogin extends AppCompatActivity {
+    private Button registratiButton;
+    private EditText Username;
+    private EditText Password;
+    private Button Login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_effettua_login);
+
+        Username = (EditText) findViewById(R.id.Username);
+        Password = (EditText) findViewById(R.id.Password);
+        Login = (Button) findViewById(R.id.loginButton);
+        registratiButton = (Button) findViewById(R.id.registratiButton);
+
+        registratiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRegistrazione();
+            }
+        });
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Username.getText().toString(), Password.getText().toString());
+            }
+        });
+
+
+    }
+
+    public void validate(String user, String pass) {
+        if((user.equals("Utente")) && (pass.equals("password"))) {
+            Intent intent1 = new Intent(this, HomepageActivity.class);
+            startActivity(intent1);
+        }else{
+            if((user.equals("")) && (pass.equals(""))) {
+                AlertDialog.Builder vuoto = new AlertDialog.Builder(this);
+                vuoto.setTitle("Campi vuoti");
+                vuoto.setMessage("Riempi i campi di Login e Password!");
+                AlertDialog alert = vuoto.create();
+                alert.show();
+            }else {
+                AlertDialog.Builder errati = new AlertDialog.Builder(this);
+                errati.setTitle("Credenziali errate");
+                errati.setMessage("Reinserisci le tue credenziali!");
+                AlertDialog alert1 = errati.create();
+                alert1.show();
+            }
+        }
+    }
+
+    public void openRegistrazione() {
+        Intent intent = new Intent(this, Registrazione.class);
+        startActivity(intent);
     }
 }
